ab41eb08377edac8b2d90b14021cb13d1e3219a9
Jim Beck <jebeck@zoominternet.net>
2026-02-16 20:36:04 -0500

    Added producer / consumer example.

---
+++ ProducerConsumer/ProducerConsumer/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ProducerConsumer
+{
+    public class Program
+    {
+
+        public static void ProducerFunction(string name)
+        {
+            var rng = new Random();
+            var buffer = ThreadSafeBuffer.Instance;
+            int count = 0;
+            while (count++ < 3)
+            {
+                buffer.Push(name, buffer.NextItem);
+
+                int sleepSec = rng.Next() % 6 + 1;
+                int sleepMs = 1000 * sleepSec;
+
+                Thread.Sleep(sleepMs);
+            }
+        }
+
+        public static void ConsumerFunction(string name)
+        {
+            var rng = new Random();
+            var buffer = ThreadSafeBuffer.Instance;
+            int count = 0;
+            while (count++ < 3)
+            {
+                var item = buffer.Pop(name);
+
+                int sleepSec = rng.Next() % 6 + 1;
+                int sleepMs = 1000 * sleepSec;
+
+                Thread.Sleep(sleepMs);
+            }
+        }
+
+
+        public static async Task Main(string[] args)
+        {
+            // Get a handle to the buffer
+            var buffer = ThreadSafeBuffer.Instance;
+
+            // List of worker tasks
+            List<Task> workers = new();
+
+            // Let's go!
+            Console.WriteLine($"Starting. {buffer.ToString()}");
+
+            // Create some producer threads
+            for (int i = 0; i < 3; i++)
+            {
+
+                string name = "Cook-" + i;
+                var t = Task.Run(() => ProducerFunction(name));
+                workers.Add(t);
+            }
+
+            // Create some consumer threads
+            for (int i = 0; i < 3; i++)
+            {
+                string name = "Eater-" + i;
+                var t = Task.Run(() => ConsumerFunction(name));
+                workers.Add(t);
+            }
+
+            // Wait for all the workers to complete
+            Task.WaitAll(workers.ToArray());
+
+            // All done. 
+            Console.WriteLine($"Finished. {buffer.ToString()}");
+        }
+    }
+} 
---
+++ ProducerConsumer/ProducerConsumer/TheadSafeBuffer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class ThreadSafeBuffer
+    {
+        static readonly int CAPACITY = 3;
+
+        /// <summary>
+        /// Get a handle to the singleton instance
+        /// </summary>
+        public static ThreadSafeBuffer Instance
+        {
+            get
+            {
+                return sInstance;
+            }
+        }
+
+        // Property that returns the next sequential item
+        public string NextItem
+        {
+            get
+            {
+                lock(mLock)
+                {
+                    return "Item-" + mNextItemNumber++;
+                }
+            }
+        }
+        
+        // Blocks the caller until a slot is available and the appends an item to the buffer
+        public void Push(string username, string item)
+        {
+            // Wait for a slot
+            mSlotsSemaphore.WaitOne();
+
+            // Push the item to the back
+            lock(mLock)
+            {
+                // Append the item
+                mBuffer.Enqueue(item);
+                
+                // Signal that an item is available 
+                mItmesSemaphore.Release();
+
+                Console.WriteLine($"\"{username}\" cooked \"{item}\"");
+            }
+        }
+
+        // Blocks the caller until an item is available and then pops it from the buffer
+        public string Pop(string username)
+        {
+            // Wait for an item
+            mItmesSemaphore.WaitOne();
+
+            lock(mLock)
+            {
+                // Get an item
+                var item = mBuffer.Dequeue();
+
+                // Signal that a slot is now available
+                mSlotsSemaphore.Release();
+
+                Console.WriteLine($"\"{username}\" ate \"{item}\"");
+
+                // Return the tiem
+                return item;
+            }
+        }
+
+        // Returns the count of items in the queue
+        public int Count()
+        {
+            lock (mLock)
+            {
+                return mBuffer.Count;
+            }
+        }
+
+        public override string ToString()
+        {
+            lock (mLock)
+            {
+                var result = $"[{string.Join(", ", mBuffer)}]";
+                return result;
+            }
+        }
+
+
+        // Private constructor.  This is a singleton
+        private ThreadSafeBuffer() { }
+
+        // Singleton instance, created in a thread-safe way w/out a lock
+        private static readonly ThreadSafeBuffer sInstance = new();
+
+        // Semaphore for slots
+        private Semaphore mSlotsSemaphore = new(CAPACITY, CAPACITY);
+
+        // Semaphore for items
+        private Semaphore mItmesSemaphore = new(0, CAPACITY);
+
+        // lock
+        private object mLock = new object();
+
+        // Fixed size queue
+        private Queue<string> mBuffer = new(CAPACITY);
+
+        // The next item number
+        private int mNextItemNumber = 0;
+    }
+}
